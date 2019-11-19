f66bbf77c135e5b190086754e00a31a3141f5954
Mohamed Ouasti <ouastim@gmail.com>
2019-11-19 17:41:09 +0100

    L'edit et la show pour la form sont faite

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :profiles do
+  resources :profiles, only: [:index, :show, :edit, :update] do
     resources :services, only: [:new, :create]
   end
   # resources :services, only: [:edit, :update]
