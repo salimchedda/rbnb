b59940030eaaf740bb905ba938df022d0ecd0e00
Mohamed Ouasti <ouastim@gmail.com>
2019-11-19 12:37:37 +0100

    Base de données fake faite

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 100 fake users...'
+100.times do
+
+  user = User.new(
+    name:    Faker::Name.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    price: rand(100..500),
+    simple_description: "#{Faker::Lorem.sentence}",
+    detailed_description: "#{Faker::Lorem.paragraphs}",
+    email: "#{Faker::Name.first_name}@#{Faker::Name.last_name}.com",
+    password: "123456",
+    keyword: "#{["front-end", "back_end", "full-stack"][rand(0..2)]}"
+  )
+  user.save!
+end
+puts 'Finished!'
+
+
+puts 'Creating 100 fake services...'
+100.times do
+
+  service = Service.new(
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    status: "#{["booked", "made", "paid"][rand(0..2)]}",
+    teacher_id: "123T3T3",
+    student_id: "23Y3YI3"
+  )
+  service.save!
+end
+puts 'Finished!'
+
+
+
+
+
+
+
