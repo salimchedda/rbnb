1c42b79213a0022a31fb3e94e511e4a30275d8c7
Salim Cheddadi <salim.cheddadi@gmail.com>
2019-11-19 17:58:37 +0100

    dashbord front 2

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,6 @@
 class ServicesController < ApplicationController
   def index
-    # @services = raise
+    @booked_services = current_user.booked_services
+    @received_services = current_user.received_services
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :services, as: :student, class_name: 'Service'
+
+  has_many :booked_services, class_name: 'Service', foreign_key: 'student_id'
+  has_many :received_services, class_name: 'Service', foreign_key: 'teacher_id'
+
   validates :name, :address, presence: true
   validates :name, uniqueness: true
 end
